29b1a962e5b32cd51defedb8ab0ee9704168105c
RamilNigamatulin <163241991+RamilNigamatulin@users.noreply.github.com>
2024-09-25 23:07:27 +0200

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -156,7 +156,7 @@ CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
 CELERY_BEAT_SCHEDULE = {
     'block_users': {
         'task': 'materials.tasks.block_users',
-        'schedule': timedelta(days=0),
+        'schedule': timedelta(days=1),
     },
 }
 
